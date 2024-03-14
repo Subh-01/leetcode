c7d9a31f94fa87886e97014bf7163c1187c1d25d
Subham Singh <37765391+Subh-01@users.noreply.github.com>
2024-03-14 23:06:00 +0530

    Time: 1 ms (100.00%), Space: 47.7 MB (61.76%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,18 +1,21 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int n = nums.length;
-        int left = 0, right = 0;
-        int count = 0, sum = 0;
-        for (; right < n; right++) {
-            sum += nums[right];
-            while (left < right && sum > goal) {
-                sum -= nums[left++];
-            }
-            if (sum == goal) count++;
-            for (int i = left; sum == goal && i < right && nums[i] == 0; i++)
-                count++;
+        return atmostSum(nums,goal)-atmostSum(nums,goal-1);
+    }
+    public int atmostSum(int arr[],int goal){
+        if(goal<0) return 0;
+//This function will give us the number of subarrays which is having the sum<=goal, means atmost the //sum can be goal, but additionally it will also count the subarrays having sum<goal.
+       int i=0,j=0,sum=0,c=0;
+        while(j<arr.length){
+            sum+=arr[j];
+            while(sum>goal)
+                sum-=arr[i++];
+           
+/*we do res = max(res, j-i+1) since there we have to find a max/min length subarray. Here we have to find total subarrays with sum<=k and when there is a valid window, the number of subarrays with sum<=k it will give is (j-i+1). Since if total sum of subarray is less than k, then all subarrays of this subarray will also have sum less than or equal to k. Hence we add that value to our answer.
+*/
+            c+=(j-i+1);
+            j++;
         }
-
-        return count;
+        return c;
     }
 }
