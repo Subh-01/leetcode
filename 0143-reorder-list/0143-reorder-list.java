2d7c1626d198bb3cdaa29550a64c173d61d78230
Subham Singh <37765391+Subh-01@users.noreply.github.com>
2024-03-23 22:58:43 +0530

    Time: 2 ms (88.69%), Space: 47.9 MB (74.69%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -10,37 +10,54 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-       if(head==null||head.next==null) return;
-        
-       ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow =slow.next;
-            fast=fast.next.next;
-        }
-        //ListNode mid=slow.next;
-        
-         //Start reverse from mid one by one 1->2->3->4->5->6 to 1->2->3->6->5->4
-        ListNode preMiddle=slow;
-            ListNode preCurrent=slow.next;
-        
-            while(preCurrent.next!=null){
-                ListNode current=preCurrent.next; 
-                preCurrent.next=current.next;
-                current.next=preMiddle.next;
-                preMiddle.next=current;
-            }
-         //Start reorder one by one  1->2->3->6->5->4 to 1->6->2->5->3->4
-         slow=head;
-            fast=preMiddle.next;
-            while(slow!=preMiddle){
-                preMiddle.next=fast.next;
-                fast.next=slow.next;
-                slow.next=fast;
-                slow=fast.next;
-                fast=preMiddle.next;
-            }
-        
-        
+        if (head == null || head.next == null)
+          return;
+      
+      // step 1. cut the list to two halves
+      // prev will be the tail of 1st half
+      // slow will be the head of 2nd half
+      ListNode prev = null, slow = head, fast = head, l1 = head;
+      
+      while (fast != null && fast.next != null) {
+        prev = slow;
+        slow = slow.next;
+        fast = fast.next.next;
+      }
+      
+      prev.next = null;
+      
+      // step 2. reverse the 2nd half
+      ListNode l2 = reverse(slow);
+      
+      // step 3. merge the two halves
+      merge(l1, l2);
     }
+    
+    ListNode reverse(ListNode head) {
+      ListNode prev = null, curr = head, next = null;
+      
+      while (curr != null) {
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+      }
+      
+      return prev;
+    }
+    
+    void merge(ListNode l1, ListNode l2) {
+      while (l1 != null) {
+        ListNode n1 = l1.next, n2 = l2.next;
+        l1.next = l2;
+        
+        if (n1 == null)
+          break;
+            
+        l2.next = n1;
+        l1 = n1;
+        l2 = n2;
+      }
+    }
+    
 }
